4368304e24f23c4048422d371d93a97b224ca97e
Georgi Georgiev <georgi@lab08.com>
2018-04-30 15:49:01 +0300

    Add custom style for labels alignment

--- README.md
+++ README.md
@@ -113,6 +113,7 @@ onPageChange(position){
 | ```labelColor``` | String  | '#000000'
 | ```currentStepLabelColor``` | String  | '#4aae4f'
 | ```labelSize``` | Number  | 13
+| ```labelAlign``` | String  | 'center'
 
 
 ### Contributing
--- StepIndicator.js
+++ StepIndicator.js
@@ -41,6 +41,7 @@ export default class StepIndicator extends PureComponent {
       stepIndicatorLabelUnFinishedColor: 'rgba(255,255,255,0.5)',
       labelColor: '#000000',
       labelSize: 13,
+      labelAlign: 'center',
       currentStepLabelColor: '#4aae4f'
     };
 
@@ -177,7 +178,10 @@ export default class StepIndicator extends PureComponent {
       });
 
       return(
-        <View style={[styles.stepLabelsContainer, direction === 'vertical' ? {flexDirection: 'column', paddingHorizontal:4} : {flexDirection: 'row', paddingVertical:4}]}>
+        <View style={[styles.stepLabelsContainer,
+          direction === 'vertical' ? {flexDirection: 'column', paddingHorizontal:4} : {flexDirection: 'row', paddingVertical:4},
+          {alignItems: this.customStyles.labelAlign}
+        ]}>
           {labelViews}
         </View>
       )
@@ -293,7 +297,6 @@ export default class StepIndicator extends PureComponent {
       backgroundColor:'transparent'
     },
     stepLabelsContainer: {
-      alignItems:'center',
       justifyContent:'space-around'
     },
     step: {
