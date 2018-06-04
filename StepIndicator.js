453e88459838603d5aed7bac3ffbcc7bacd15cdd
Dani Lazarov <danny.lazarow@gmail.com>
2018-06-04 12:09:22 +0300

    fix PropTypes (now react use separate module), bump version

--- StepIndicator.js
+++ StepIndicator.js
@@ -1,5 +1,5 @@
-
-import React, { PureComponent,PropTypes } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types'
 import { View,Text,StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native';
 
 const STEP_STATUS = {
