112cc022eaa51637375f27e2d059c6973cb4e421
Arkit Vora <arkitvora@Arkits-MacBook-Pro.local>
2019-02-05 21:14:24 +0530

    Updating example to showcase separatorStrokeFinishedWidth

--- example/HorizontalStepIndicatorExample.js
+++ example/HorizontalStepIndicatorExample.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { AppRegistry,StyleSheet,View, Text } from 'react-native';
-import { ViewPager } from 'rn-viewpager';
+import React, { Component } from 'react'
+import { AppRegistry, StyleSheet, View, Text } from 'react-native'
+import { ViewPager } from 'rn-viewpager'
 
-import StepIndicator from 'react-native-step-indicator';
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import StepIndicator from 'react-native-step-indicator'
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 
-const PAGES = ['Page 1','Page 2','Page 3','Page 4','Page 5'];
+const PAGES = ['Page 1', 'Page 2', 'Page 3', 'Page 4', 'Page 5']
 
 const firstIndicatorStyles = {
   stepIndicatorSize: 30,
-  currentStepIndicatorSize:40,
+  currentStepIndicatorSize: 40,
   separatorStrokeWidth: 3,
   currentStepStrokeWidth: 5,
   separatorFinishedColor: '#4aae4f',
@@ -29,11 +29,12 @@ const firstIndicatorStyles = {
 
 const secondIndicatorStyles = {
   stepIndicatorSize: 30,
-  currentStepIndicatorSize:40,
+  currentStepIndicatorSize: 40,
   separatorStrokeWidth: 2,
   currentStepStrokeWidth: 3,
   stepStrokeCurrentColor: '#fe7013',
   stepStrokeWidth: 3,
+  separatorStrokeFinishedWidth: 4,
   stepStrokeFinishedColor: '#fe7013',
   stepStrokeUnFinishedColor: '#aaaaaa',
   separatorFinishedColor: '#fe7013',
@@ -53,7 +54,7 @@ const secondIndicatorStyles = {
 
 const thirdIndicatorStyles = {
   stepIndicatorSize: 25,
-  currentStepIndicatorSize:30,
+  currentStepIndicatorSize: 30,
   separatorStrokeWidth: 2,
   currentStepStrokeWidth: 3,
   stepStrokeCurrentColor: '#7eaec4',
@@ -76,101 +77,126 @@ const thirdIndicatorStyles = {
 }
 
 const getStepIndicatorIconConfig = ({ position, stepStatus }) => {
-    const iconConfig = {
-      name: 'feed',
-      color: stepStatus === 'finished' ? '#ffffff' : '#fe7013',
-      size: 15,
-    };
-    switch (position) {
-      case 0: {
-        iconConfig.name = 'shopping-cart';
-        break;
-      }
-      case 1: {
-        iconConfig.name = 'location-on';
-        break;
-      }
-      case 2: {
-        iconConfig.name = 'assessment';
-        break;
-      }
-      case 3: {
-        iconConfig.name = 'payment';
-        break;
-      }
-      case 4: {
-        iconConfig.name = 'track-changes';
-        break;
-      }
-      default: {
-        break;
-      }
+  const iconConfig = {
+    name: 'feed',
+    color: stepStatus === 'finished' ? '#ffffff' : '#fe7013',
+    size: 15
+  }
+  switch (position) {
+    case 0: {
+      iconConfig.name = 'shopping-cart'
+      break
     }
-    return iconConfig;
-  };
+    case 1: {
+      iconConfig.name = 'location-on'
+      break
+    }
+    case 2: {
+      iconConfig.name = 'assessment'
+      break
+    }
+    case 3: {
+      iconConfig.name = 'payment'
+      break
+    }
+    case 4: {
+      iconConfig.name = 'track-changes'
+      break
+    }
+    default: {
+      break
+    }
+  }
+  return iconConfig
+}
 
 export default class App extends Component {
-
-  constructor() {
-    super();
+  constructor () {
+    super()
     this.state = {
-      currentPage:0
+      currentPage: 0
     }
   }
 
-  componentWillReceiveProps(nextProps,nextState) {
-    if(nextState.currentPage != this.state.currentPage) {
-      if(this.viewPager) {
+  componentWillReceiveProps (nextProps, nextState) {
+    if (nextState.currentPage != this.state.currentPage) {
+      if (this.viewPager) {
         this.viewPager.setPage(nextState.currentPage)
       }
     }
   }
 
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         <View style={styles.stepIndicator}>
-          <StepIndicator customStyles={firstIndicatorStyles} currentPosition={this.state.currentPage} labels={["Account","Profile","Band","Membership","Dashboard"]} />
+          <StepIndicator
+            customStyles={firstIndicatorStyles}
+            currentPosition={this.state.currentPage}
+            labels={['Account', 'Profile', 'Band', 'Membership', 'Dashboard']}
+          />
         </View>
         <View style={styles.stepIndicator}>
-          <StepIndicator renderStepIndicator={this.renderStepIndicator} customStyles={secondIndicatorStyles} currentPosition={this.state.currentPage} labels={["Cart","Delivery Address","Order Summary","Payment Method","Track"]} />
+          <StepIndicator
+            renderStepIndicator={this.renderStepIndicator}
+            customStyles={secondIndicatorStyles}
+            currentPosition={this.state.currentPage}
+            labels={[
+              'Cart',
+              'Delivery Address',
+              'Order Summary',
+              'Payment Method',
+              'Track'
+            ]}
+          />
         </View>
         <View style={styles.stepIndicator}>
-          <StepIndicator stepCount={4} customStyles={thirdIndicatorStyles} currentPosition={this.state.currentPage} labels={["Approval","Processing","Shipping","Delivery"]} />
+          <StepIndicator
+            stepCount={4}
+            customStyles={thirdIndicatorStyles}
+            currentPosition={this.state.currentPage}
+            labels={['Approval', 'Processing', 'Shipping', 'Delivery']}
+          />
         </View>
         <ViewPager
-          style={{flexGrow:1}}
-          ref={(viewPager) => {this.viewPager = viewPager}}
-          onPageSelected={(page) => {this.setState({currentPage:page.position})}}
-          >
-            {PAGES.map((page) => this.renderViewPagerPage(page))}
-          </ViewPager>
+          style={{ flexGrow: 1 }}
+          ref={viewPager => {
+            this.viewPager = viewPager
+          }}
+          onPageSelected={page => {
+            this.setState({ currentPage: page.position })
+          }}
+        >
+          {PAGES.map(page => this.renderViewPagerPage(page))}
+        </ViewPager>
       </View>
-    );
+    )
   }
 
-  renderViewPagerPage = (data) => {
-    return(<View style={styles.page}>
-      <Text>{data}</Text>
-    </View>)
+  renderViewPagerPage = data => {
+    return (
+      <View style={styles.page}>
+        <Text>{data}</Text>
+      </View>
+    )
   }
 
   renderStepIndicator = params => (
     <MaterialIcon {...getStepIndicatorIconConfig(params)} />
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#ffffff'
   },
   stepIndicator: {
-    marginVertical:50,
+    marginVertical: 50
   },
   page: {
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
-});
+})
